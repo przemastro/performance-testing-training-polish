88142fe1875c916195beb59864878ab45bbe017e
Przemek <przemyslaw.jagodzinski@impaqgroup.com>
2019-05-25 21:20:59 +0200

    added full data, recession tables and reports with join

--- MongoDB_InsertCollections.bat
+++
@@ -1,5 +0,0 @@
-ECHO OFF
-REM cd \Program Files\MongoDB\Server\4.0\bin
-For /F "delims=, tokens=1,2,3,4,5" %%G IN (C:\Users\pjag\Desktop\Performance\StagingArea\YieldCurveView.csv) DO (
-   mongo 127.0.0.1:27017/Economy -u pjag -p admin --eval "db.YieldCurve.insert({'ObservationDate': '%%G', 'T10Y3M': %%H, 'T10Y2Y': %%I, 'Trend_T10Y3M': %%J, 'Trend_T10Y2Y': %%K});"
-)
